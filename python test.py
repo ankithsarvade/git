7a8ec83afdeba8e4138b52830f665eb5538be96a
ankithsarvade <ankithsarvade96@gmail.com>
2025-03-26 12:48:00 +0530

    Update python test.py

--- python test.py
+++ python test.py
@@ -1,1 +1,22 @@
 
+pipeline {
+    agent any 
+    stages {
+        stage('Build') { 
+            steps {
+                echo "stage-1"
+            }
+        }
+        stage('Test') { 
+            steps {
+            echo "stage-2"
+            }
+        }
+        stage('Deploy') { 
+            steps {
+                echo "stage-3"
+            }
+        }
+    }
+}
+
